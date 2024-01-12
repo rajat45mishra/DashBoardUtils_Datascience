2ba78768ea3f5f6853bff592002186de4fc1104e
Rajat Mishra <mishra.rajat855@gmail.com>
2024-01-12 16:01:00 +0530

    Docs update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 long_description = open("README.md", "rb").read().decode()
 setup(
     name="DashBoardUtils-DataScience",
-    version="1.26",
+    version="1.27",
     author="Rajat Mishra",
     author_email="rajatsmishra@aol.com",
     description="AutoMated visualization Features Extraction For Data Scientists and data format calculater for application developers",
